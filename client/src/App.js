7a85b2223ca613478492fbdf71514c524f6dc726
imxande <alerodriguezlarosa@gmail.com>
2019-11-12 17:24:17 -0500

    part 1 competed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-
+import {Route} from "react-router-dom";
+import MovieList from "../src/Movies/MovieList";
+import Movie from '../src/Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -12,7 +14,10 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path="/" component= {MovieList}/>
+        <Route exact path="/movies/:id" component= {Movie}/>
+      </div>
     </div>
   );
 };
